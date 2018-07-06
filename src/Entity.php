e5fa5ee6dc97b13f84aef8835e4d034da5cfad10
Unknown <hululidexiong@163.com>
2018-07-06 10:14:57 +0800

    加入entity的默认值

--- src/Entity.php
+++ src/Entity.php
@@ -10,8 +10,26 @@ namespace MedMy;
 
 class Entity{
 
+
+
+    public $_option = [
+        'is_filter_null' => true,
+        'get_attr_strip' => [],
+        'get_field' => []
+    ];
+
+    function __construct()
+    {
+        $this->_option['get_attr_strip'] = $this->get_attr_strip();
+    }
+
     //首次创建表时插入的数据
     function default_data(){
         return [];
+    }
+
+    //getAttribute 不会获取的数据
+    function get_attr_strip(){
+        return ['id' , 'inputtime'];
     }
 }
